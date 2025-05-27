c873da8bfc65542f96c56380d1c47a39f48e9d30
raflyzainn <112810094+raflyzainn@users.noreply.github.com>
2025-05-27 19:10:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,17 @@ st.title(':mortar_board: Student Performance in Exams by Machine Learning')
 
 st.info('📊 Student Performance Analysis by Machine Learning')
 
+df['average_score'] = df[['math score', 'reading score', 'writing score']].mean(axis=1)
+
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv("StudentsPerformance.csv")
   df
+
+  X = df.drop(columns=['average_score'])
+  st.write("🧩 **Features (X)**")
+  st.dataframe(X)
+
+  y = df['average_score']
+  st.write("🎯 **Target (y)**")
+  st.dataframe(y)
