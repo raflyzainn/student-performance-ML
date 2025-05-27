ff3aa69686047b6527ada93c6df3245ae56cb0b6
raflyzainn <112810094+raflyzainn@users.noreply.github.com>
2025-05-27 19:07:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title(':mortar_board: Student Performance in Exams by Machine Learning')
 st.info('📊 Student Performance Analysis by Machine Learning')
 
 with st.expander('Data'):
-  st.write('**Raw Data**)
+  st.write('**Raw Data**')
   df = pd.read_csv("StudentsPerformance.csv")
   df
